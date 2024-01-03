7860000ad8a02e019fa2068d79ddfaa55b836ccc
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-03 15:33:03 +0000

    Modify output to match expected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+    .then((values) => values.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : String(res.reason),
+    })));
 }
