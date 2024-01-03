10b2f7195243406024a5a2c5b15c0ede4b10d0d8
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-03 13:10:21 +0000

    Call necessary functions in all expression

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
-
-  Promise.all([photoPromise, userPromise])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
