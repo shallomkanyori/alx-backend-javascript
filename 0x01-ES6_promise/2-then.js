cb989dcc93c643b462ed15ec00f06665d947f035
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-03 12:38:33 +0000

    Use a single then block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .then(() => { console.log('Got a response from the API'); });
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }, () => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
