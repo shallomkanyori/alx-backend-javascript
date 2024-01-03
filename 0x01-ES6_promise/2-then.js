ded6691c0f2040e9f659d011ec739a3ddf0137ba
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-03 12:43:45 +0000

    Only log to the console when Promise is resolved

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,5 @@ export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'success' };
-  }, () => {
-    console.log('Got a response from the API');
-    return new Error();
-  });
+  }, () => new Error());
 }
