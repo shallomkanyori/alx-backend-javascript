88cf1a83d013699368e17bacde97c9c9ec663577
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-03 13:52:24 +0000

    Use catch

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const photo = values[0];
-      const user = values[1];
-
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    }, () => { console.log('Signup system offline'); });
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
