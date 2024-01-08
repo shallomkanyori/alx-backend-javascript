d5b86e4a7625434465f23b81536b830f1238c728
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-08 16:15:27 +0000

    Add JS function that updates a Map
    Add a JS function that takes in a Map and returns an updated Map with
    all keys with a value of 1 updated to have a value of 100.

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+
+  map.forEach((val, key) => {
+    if (val === 1) {
+      map.set(key, 100);
+    }
+  });
+
+  return map;
+}
