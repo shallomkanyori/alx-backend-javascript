f12801a3a90f8785da2ff009f2e8a47ab6242d9f
Shallom Kanyori <shallomkanyori@gmail.com>
2024-01-08 17:49:23 +0000

    Add type checks and null checks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,18 @@
 export default function cleanSet(set, startString) {
-  const res = [];
-  if (startString) {
-    set.forEach((el) => {
-      if (el.startsWith(startString)) {
-        res.push(el.replace(startString, ''));
-      }
-    });
+  if (!set || !startString) {
+    return '';
   }
+
+  if (!(set instanceof Set) || (typeof startString !== 'string')) {
+    return '';
+  }
+
+  const res = [];
+  set.forEach((el) => {
+    if (el && el.startsWith(startString)) {
+      res.push(el.replace(startString, ''));
+    }
+  });
 
   return res.join('-');
 }
