b8b0f4a87272ae132ded44286aaef42eb9c7097c
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-26 16:09:15 +0000

    Use named export

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,7 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
+module.exports.app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-});
-
-
-app.listen(1245);
-module.exports = app;
+}).listen(1245);
