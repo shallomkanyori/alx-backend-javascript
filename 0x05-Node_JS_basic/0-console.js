1c13052a5ab2646e4417a4df46683d0b8fa10bbc
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-26 11:33:55 +0000

    Implement JS function that prints string to stdout

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a string to STDOUT
+ * @param {String} message The string to print
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
--- 0x05-Node_JS_basic/README.md
+++ 0x05-Node_JS_basic/README.md
@@ -1,1 +1,6 @@
 # NodeJS Basics
+
+## Tasks
+
+### Task 0
+In the file [0-console.js](0-console.js), create a function named `displayMessage` that prints in STDOUT the string argument.
