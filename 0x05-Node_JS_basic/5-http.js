026f8e8bc790061e442e164eb3935d1fff77c72d
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-26 19:12:27 +0000

    Extract reading from file into function

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,54 +1,63 @@
 const http = require('http');
 const fsPromises = require('fs').promises;
 
-if (process.argv.length < 3) {
-  console.log('Usage: node 5-http.js FILENAME');
-  process.exit(1);
-}
+const dbFile = process.argv.length > 2 ? process.argv[2] : '';
 
-const dbFile = process.argv[2];
+/**
+ * Prints the count of the number of student records from a database csv file
+ * @param {String} path The path to the database csv file
+ */
+function countStudents(path) {
+  return fsPromises.readFile(path, 'utf-8').then((data) => {
+    const lines = data.split(/\r\n|\n/).slice(1);
+    let studentCount = 0;
+    const studentsByField = new Map();
+
+    for (const line of lines) {
+      if (line) {
+        studentCount += 1;
+        const info = line.split(',');
+        const firstname = info[0];
+        const field = info[info.length - 1];
+
+        const s = studentsByField.get(field) || [];
+        s.push(firstname);
+        studentsByField.set(field, s);
+      }
+    }
+
+    const res = [];
+    res.push(`Number of students: ${studentCount}`);
+    studentsByField.forEach((students, field) => {
+      res.push(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    });
+
+    return res.join('\n');
+  }, () => {
+    throw new Error('Cannot load the database');
+  });
+}
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
   switch (req.url) {
     case '/students':
-      fsPromises.readFile(dbFile, 'utf-8').then((data) => {
+      res.write('This is the list of our students\n');
+      countStudents(dbFile).then((data) => {
         res.statusCode = 200;
-        res.write('This is the list of our students\n', 'utf-8');
-
-        const lines = data.split(/\r\n|\n/).slice(1);
-        let studentCount = 0;
-        const studentsByField = new Map();
-
-        for (const line of lines) {
-          if (line) {
-            studentCount += 1;
-            const info = line.split(',');
-            const firstname = info[0];
-            const field = info[info.length - 1];
-
-            const s = studentsByField.get(field) || [];
-            s.push(firstname);
-            studentsByField.set(field, s);
-          }
-        }
-
-        res.write(`Number of students: ${studentCount}\n`, 'utf-8');
-        let rstr = '';
-        studentsByField.forEach((students, field) => {
-          rstr += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
-        });
-        rstr = rstr.slice(0, -1);
-        res.end(rstr);
-      }).catch(() => {
+        res.end(data);
+      }).catch((err) => {
         res.statusCode = 500;
-        res.end('Cannot load the database');
+        res.end(err.message);
       });
+
       break;
+
     default:
       res.statusCode = 200;
       res.end('Hello Holberton School!');
+
       break;
   }
 }).listen(1245);
