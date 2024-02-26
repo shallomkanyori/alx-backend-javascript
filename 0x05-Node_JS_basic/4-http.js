17a0bb4a8fe2b0f442939ef4b8e1fed41cb290f1
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-26 16:12:16 +0000

    Export at the end of the file

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
-module.exports.app = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 }).listen(1245);
+
+module.exports = app;
