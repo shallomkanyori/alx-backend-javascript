f532b84fc390f5a180255fd9e0851c3a2007e542
shallomkanyori <shallomkanyori@gmail.com>
2024-02-28 19:18:28 +0300

    Use correct version of sinon.spy()

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,7 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function () {
-    const spy = sinon.spy(Utils.calculateNumber);
+    const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
 
     expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
