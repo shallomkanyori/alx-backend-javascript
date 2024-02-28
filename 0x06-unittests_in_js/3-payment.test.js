297379a69650328eae505c784a9a2eac2f511e87
shallomkanyori <shallomkanyori@gmail.com>
2024-02-28 19:20:59 +0300

    Fix test to be inside it callback

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,10 +7,12 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function () {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 20);
-
-    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-
-    spy.restore();
+    it('should call Utils.calculateNumber', function () {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 20);
+        
+        expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        
+        spy.restore();
+    });
 });
