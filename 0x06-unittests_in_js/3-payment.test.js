1a2807fee38c79555500fbe1e7654a6e7d018bf7
shallomkanyori <shallomkanyori@gmail.com>
2024-02-28 18:58:14 +0300

    Use sinon.spy as a wrapper

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,10 +7,10 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function () {
-    Utils.calculateNumber = sinon.spy();
+    const spy = sinon.spy(Utils.calculateNumber);
     sendPaymentRequestToApi(100, 20);
 
-    expect(Utils.calculateNumber.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
 
-    Utils.calculateNumber.restore();
+    spy.restore();
 });
