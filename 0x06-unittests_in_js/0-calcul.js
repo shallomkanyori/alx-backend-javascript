764092413975bcbae2df095faecfe2ae71c7edd2
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-28 17:40:24 +0000

    Add semicolon

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -2,10 +2,10 @@
  * Rounds two numbers and returns their sum
  * @param {Number} a The first number
  * @param {Number} b The second number
- * @return {Number} Sum of round(a) and round(b)
+ * @returns {Number} Sum of round(a) and round(b)
  */
 const calculateNumber = function (a, b) {
 	return Math.round(a) + Math.round(b);
-}
+};
 
 module.exports = calculateNumber;
