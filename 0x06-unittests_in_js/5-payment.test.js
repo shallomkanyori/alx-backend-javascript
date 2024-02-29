ee38af5cbb0d0ac73b58455b52ae86e4a2262e7f
shallomkanyori <shallomkanyori@gmail.com>
2024-02-29 10:47:57 +0300

    Use correct sum values

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -19,11 +19,11 @@ describe('sendPaymentRequestToApi', function () {
 
     it('should log "The total is: 120" for args (100, 20)', function () {
         sendPaymentRequestToApi(100, 20);
-        expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+        expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
     });
 
     it('should log "The total is: 20" for args (10, 10)', function () {
         sendPaymentRequestToApi(10, 10);
-        expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+        expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
     });
 });
